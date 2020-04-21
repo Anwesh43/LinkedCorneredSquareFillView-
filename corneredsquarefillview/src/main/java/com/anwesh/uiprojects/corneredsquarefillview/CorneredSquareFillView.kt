fb3a7ba4e86bcaf1849f2bc551c05803e9b99e55
Anwesh43 <anweshthecool0@gmail.com>
2020-04-22 03:17:22 +0530

    adding Renderer class

--- corneredsquarefillview/src/main/java/com/anwesh/uiprojects/corneredsquarefillview/CorneredSquareFillView.kt
+++ corneredsquarefillview/src/main/java/com/anwesh/uiprojects/corneredsquarefillview/CorneredSquareFillView.kt
@@ -182,4 +182,27 @@ class CorneredSquareFillView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CorneredSquareFillView) {
+
+        private val animator : Animator = Animator(view)
+        private val csf : CorneredSquareFill = CorneredSquareFill(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            csf.draw(canvas, paint)
+            animator.animate {
+                csf.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            csf.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
