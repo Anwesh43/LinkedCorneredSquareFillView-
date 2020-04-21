d5badbf73a6cce4bce8d0281f07f518b45e61a49
Anwesh43 <anweshthecool0@gmail.com>
2020-04-22 03:26:39 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcorneredsquarefillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcorneredsquarefillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedcorneredsquarefillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.corneredsquarefillview.CorneredSquareFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CorneredSquareFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
