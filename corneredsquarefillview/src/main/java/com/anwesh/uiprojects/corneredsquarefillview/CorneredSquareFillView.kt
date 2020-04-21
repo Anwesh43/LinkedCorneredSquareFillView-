3d54367be43d9c577cf1da248b8c13518b1205ee
Anwesh43 <anweshthecool0@gmail.com>
2020-04-22 03:12:02 +0530

    adding CSFNode class

--- corneredsquarefillview/src/main/java/com/anwesh/uiprojects/corneredsquarefillview/CorneredSquareFillView.kt
+++ corneredsquarefillview/src/main/java/com/anwesh/uiprojects/corneredsquarefillview/CorneredSquareFillView.kt
@@ -118,4 +118,45 @@ class CorneredSquareFillView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSFNode(var i : Int, val state : State = State()) {
+
+        private var next : CSFNode? = null
+        private var prev : CSFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CSFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCSFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSFNode {
+            var curr : CSFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
